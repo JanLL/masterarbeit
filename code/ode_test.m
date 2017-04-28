092abf6c3f38e6fc51f63213d3638a12478333be
JanLL <lammel@stud.uni-heidelberg.de>
2017-04-28 15:45:05 +0200

    updated length to more realistic size and improved plot visualization

--- code/ode_test.m
+++ code/ode_test.m
@@ -1,10 +1,10 @@
 startup;
 
 
-T0 = 30 .* ones(N,1);
+T0 = 80 .* ones(N,1);
 
 t0 = 0.;
-tf = 200.;
+tf = 150.;
 t = linspace(t0, tf, (tf-t0)*10000)';
 
 opts = odeset('reltol', 1.0d-6, 'abstol', 1.0d-16);
@@ -12,10 +12,10 @@ sol = ode15s(@ode_system1d, t, T0, opts);
 
 T = deval(sol, t)';
 
-plot(t, T(:,1), 'g--', 'DisplayName', 'T_0'); hold on
-plot(t, T(:,2), 'r--', 'DisplayName', 'T_1'); hold on
-plot(t, T(:,3), 'y--', 'DisplayName', 'T_2'); hold on
-plot(t, T(:,end), 'b--', 'DisplayName', 'T_N'); hold on
+plot(t, T(:,1) - T(:,1), 'g--', 'DisplayName', 'T_0'); hold on
+plot(t, T(:,2) - T(:,1), 'r--', 'DisplayName', 'T_1'); hold on
+plot(t, T(:,3) - T(:,1), 'y--', 'DisplayName', 'T_2'); hold on
+%plot(t, T(:,end), 'b--', 'DisplayName', 'T_N'); hold on
 
 
 % xlabel('Time t');
