e88fe1a04025b80ad65aaf5151e2284fbcb690cc
JanLL <lammel@stud.uni-heidelberg.de>
2017-07-23 01:16:56 +0200

    built approx c_p curve with nurbes, started better visualization of param values in each iteration

--- code/bugfixing.m
+++ code/bugfixing.m
@@ -2,8 +2,8 @@
 
 nrb_order = 4; % nrb_order = 4 equates to C^2
 
-cntrl_pts = [30, 60, 90, 120, 125, 130, 131, 135, 150; ...
-             1,  1.1, 1.15, 1.2, 5., 10, 1.5, 1.51, 1.52];
+cntrl_pts = [30, 60, 90, 120, 125, 130, 130., 135, 150, 160; ...
+             1,  1.1, 1.15, 1.2, 5., 10, 1.5, 1.51, 1.52, 1.53];
 num_cntrl_pts = size(cntrl_pts,2);
 
 % equidistant knots
@@ -15,27 +15,30 @@ knots = [zeros(1,nrb_order), ...
 % knots = [0.0, 0.0, 0.0, 0.0, 0.2, 0.4, 0.7, 0.8 1.0, 1.0, 1.0, 1.0];
 
 
-length(knots)
-size(cntrl_pts, 2)
-
-%assert(length(knots) - size(cntrl_pts, 2) == 3);
+assert(length(knots) - size(cntrl_pts, 2) == 4); % check for C^2 continuity
 
 curve = nrbmak(cntrl_pts, knots);
 dcurve = nrbderiv(curve);
-
            
-tt = 0:0.01:1;
-
+tt = 0:0.001:1;
 tic;
 [C,dC] = nrbdeval(curve, dcurve, tt);
 toc
 
+x = C(1,:);
+y = C(2,:);
 
-dx = dC(1,:);
-dy = dC(2,:) ./ dC(1,:);
+dy = dC(2,:) ./ dC(1,:); % gradients -> derivative
 
+x_interp = 30:0.005:160;
+
+tic;
+y_interp = interp1(x, y, x_interp);
+dy_interp = interp1(x, dy, x_interp);
+toc
 
 figure(1);
 %cla;
-plot(x,y); hold on
-plot(x, dy)
+
+plot(x_interp, y_interp); hold on
+plot(x_interp, dy_interp)
--- code/disp_aux.m
+++ code/disp_aux.m
@@ -2,10 +2,16 @@ function [stop] = disp_aux(x, optimValues, state)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+
 stop = false;
 
-disp(x);
+%disp(x);
 
+% TODO: verallgemeinern fuer beliege groesse von x ... mit 2xlength(x)
+% matrix und strcat(s(:,1)', s(:,2)')
+x = 1:0.12783213:10;
+fprintf('param number:\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n');
+fprintf(strcat('param value:\t', repmat('%1.5g\t',1,10)), x(1:10));
 
 
 end
