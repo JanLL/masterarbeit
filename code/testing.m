0345c69bc6907b666f8c3cbcc385a491aba3b2a4
JanLL <lammel@stud.uni-heidelberg.de>
2017-07-23 18:49:53 +0200

    running forward integration with c_p NURBS

--- code/testing.m
+++ code/testing.m
@@ -1,75 +1,69 @@
-% convergence of spatial discretization N1
+% forward integration with NURBS
 
 L1 = 25.;
 L2 = 0.;
 L3 = 1.;
 
-N1 = 6;
-N2 = 0;
 N3 = 50;
 
-T_0 = 10;
-T_end = 300;
+T_0 = 30;
+T_end = 180;
 
 heat_rate = 10.; % K/min
 
 lambda_test_setup = [23*1, 35.6000, 0.9600];
 
+k_sap_fit = [0.    45.5951   0.];
 
-common_args = {'L1', L1, 'L2', L2, 'L3', L3, 'N1', N1, 'N2', N2, 'N3', N3, 'T_0', T_0, ...
+% c_p parametrization of sample
+nrb_order = 4; % nrb_order = 4 equates to C^2
+
+cntrl_pts = [10, 30, 60, 90, 120, 125, 130, 131, 135, 150, 160, 200; ...
+             1, 1,  1.1, 1.15, 1.2, 5., 10, 1.5, 1.51, 1.52, 1.53, 1.58];
+num_cntrl_pts = size(cntrl_pts,2);
+
+% equidistant knots
+knots = [zeros(1,nrb_order), ...
+         (1:num_cntrl_pts-nrb_order)/(num_cntrl_pts-nrb_order+1), ...
+         ones(1,nrb_order)];
+
+c_p_sample = {'NURBS', [size(cntrl_pts,2), length(knots)]};
+
+common_args = {'L1', L1, 'L2', L2, 'L3', L3, 'N3', N3, 'T_0', T_0, ...
                'T_end', T_end, 'heat_rate', heat_rate, ...
-               'lambda_test_setup', lambda_test_setup};
+               'lambda_test_setup', lambda_test_setup, ...
+               'c_p_sample', c_p_sample};
 p_sim = get_param_sim(common_args{:});
 
+p_optim_start = [cntrl_pts(1,:), cntrl_pts(2,:), knots, k_sap_fit];
 
-c_p_params = [144.0009, ...
-                    4.1036, ...
-                    0.0039, ...
-                    1.4217, ...
-                    0.0078, ...
-                    1.5325];
+p_sim = update_c_p(p_sim, p_optim_start);
 
-p_sim = update_c_p(p_sim, c_p_params);
-
-
-T_pcm_old = simulate_1d(p_sim.eval_c_p, p_sim.eval_dc_p, p_sim);
-
-error = 1;
-N1_list = [N1];
-
-i = 1;
-for i=1:50
-
-    N1_old = N1;
-    N1 = N1 + 2;
-    p_sim.N1 = N1;
-    
-    T_pcm_new = simulate_1d(p_sim.eval_c_p, p_sim.eval_dc_p, p_sim);
-    
-    error = max(abs((T_pcm_new(:,N1) - T_pcm_old(:,N1_old)) ./ T_pcm_new(:,N1)));
-    
-    error_list(i) = error;
-    %i = i+1;
-    
-    T_pcm_old = T_pcm_new;
-    
-end
-
-
-semilogy((1:length(error_list))*2 + 6, error_list, 'x')
-xlabel('N1');
-ylabel('max\{|1 - T_{pcm}(N_1) / T_{pcm}(N_1^+)|\}');
-title(sprintf('Convergence rate of increasing Constantan\ndiscretization number'));
+% T = 30:0.01:160;
+% plot(T, p_sim.eval_c_p(T)); hold on
+% plot(T, p_sim.eval_dc_p(T));
 
 
 
+tic;
+T_pcm = simulate_1d(p_sim);
+toc
+
+
+% Analytical reference solution
+lambda_const = p_sim.lambda_test_setup(1);
+rho_const = p_sim.rho_test_setup(1);
+c_p_const = p_sim.c_p_test_setup(1);
+
+heat_rate_s = heat_rate / 60;
+dt = 0.05 / heat_rate_s; % fct evaluation every 0.05K
+t = 0:dt:1/heat_rate_s*(T_end - T_0);
+n = 100;
+a = lambda_const / (c_p_const * rho_const);
+T_ref = analytical_sol(L1,t,n,T_0, heat_rate_s, a); 
 
 
 
-
-
-
-
-
+plot(T_ref,T_ref-T_pcm(:,p_sim.N1)); hold on
 
 
--- code/update_c_p.m
+++ code/update_c_p.m
@@ -25,7 +25,26 @@ elseif strcmp(p_sim.c_p_type, 'B_spline')
     p_sim.eval_dc_p = @(T) spval(p_sim.dc_p_bspline, T); 
     
 elseif strcmp(p_sim.c_p_type, 'NURBS')
-    % TODO!
+    p_sim.c_p_nurbs.coefs(1,:) = p_sim.get_param_c_p_cntrl_pts_x(p_optim);
+    p_sim.c_p_nurbs.coefs(2,:) = p_sim.get_param_c_p_cntrl_pts_y(p_optim);
+    p_sim.c_p_nurbs.knots = p_sim.get_param_c_p_knots(p_optim);
+    
+    p_sim.dc_p_nurbs = nrbderiv(p_sim.c_p_nurbs);
+
+
+    tt = 0:0.001:1;
+    [c_p_curve,dc_p_curve] = ...
+        nrbdeval(p_sim.c_p_nurbs, p_sim.dc_p_nurbs, tt);
+
+    p_sim.c_p_T_table = c_p_curve(1,:);
+    p_sim.c_p_table = c_p_curve(2,:);
+    p_sim.dc_p_table = dc_p_curve(2,:) ./ dc_p_curve(1,:); 
+
+    % maybe not necessary...:
+    p_sim.eval_c_p = ...
+        @(T) interp1(p_sim.c_p_T_table, p_sim.c_p_table, T);
+    p_sim.eval_dc_p = ...
+        @(T) interp1(p_sim.c_p_T_table, p_sim.dc_p_table, T);
 
 else
     error('in update_c_p struct p_sim neither had c_p_type ''function_handle'' nor ''B_spline''.');
