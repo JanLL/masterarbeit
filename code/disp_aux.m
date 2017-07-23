08a5a99fdc5c5935bfdad306db172a23d06214cb
JanLL <lammel@stud.uni-heidelberg.de>
2017-07-23 13:09:18 +0200

    minor bugfix in disp_aux

--- code/disp_aux.m
+++ code/disp_aux.m
@@ -9,7 +9,7 @@ stop = false;
 
 % TODO: verallgemeinern fuer beliege groesse von x ... mit 2xlength(x)
 % matrix und strcat(s(:,1)', s(:,2)')
-x = 1:0.2273:10;
+%x = 1:0.2273:10;
 
 for j=0:ceil(length(x)/10)-1
     fprintf('param number:\t');
@@ -18,8 +18,8 @@ for j=0:ceil(length(x)/10)-1
     
     fprintf('param value:\t');
     index = (1:10)+j*10;
-    index = index(index < length(x));
-    fprintf(repmat('%1.5g\t',1,10), x(index));
+    index = index(index <= length(x));
+    fprintf(repmat('%1.3g\t',1,10), x(index));
     fprintf('\n');
 end
 
