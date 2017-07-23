abc5b312d08a59623e470b779c65e760d06e0ea6
JanLL <lammel@stud.uni-heidelberg.de>
2017-07-23 19:42:45 +0200

    c_p fit with NURBS running in principle

--- code/test_param_estimation_nurbs.m
+++ code/test_param_estimation_nurbs.m
@@ -32,17 +32,22 @@ lambda_test_setup = [23*1, 35.6000, 0.9600];
 
 optim_solverName = 'lsqnonlin';
 
-% Solve optimization problem min_p ||U_dsc - dU||_2^2
-knots = [-10,0, 30, 50, 60, 70, 80, 90, 100, 110, 115, 122, 127, 132, ...
-         135:1:160, 165, 170, 200];
-coeffs = (0.05 .* [1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 5, 15, 15, 15, 15, 15, ...
-                  15, 15, 15, 15, 15, 15, 15, 15, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5 1.5, 1.5, 1.5, 1.5, 1.5, 1.5]);
-% sqrt (later we square) to avoid negative coeffs when using optimizer without bounds
 
-% length(knots)
-% length(coeffs)
-% return
+% c_p parametrization of sample
+nrb_order = 4; % nrb_order = 4 equates to C^2
 
+cntrl_pts = [0, 30, 60, 90, 120, 125, 130:2:160, 180, 200; ...
+             1, 1,  1.1, 1.15, 1.2, 5., repmat(10,1,16), 1.5, 1.51];
+num_cntrl_pts = size(cntrl_pts,2);
+
+cntrl_pts(2,:) = cntrl_pts(2,:) .* 0.05;
+
+% equidistant knots
+knots = [zeros(1,nrb_order), ...
+         (1:num_cntrl_pts-nrb_order)/(num_cntrl_pts-nrb_order+1), ...
+         ones(1,nrb_order)];
+
+     
 if revMassNorm
     k_sap_fit = [0.    45.5951   0.]; % values from saphire-fit
 else
@@ -50,22 +55,26 @@ else
 end
 k_data_table = [3.67763861e-02   6.00028439e+01  -4.47793211e+01]; % values from data table fit
 
-p_optim_start = cat(2, knots, coeffs, k_sap_fit);
+p_optim_start = [cntrl_pts(1,:), cntrl_pts(2,:), knots, k_sap_fit];
 
-c_p_sample = {'B-', [length(knots), length(coeffs)]};
+c_p_sample = {'NURBS', [num_cntrl_pts, length(knots)]};
+
 common_args = {'L1', L1, 'L2', L2, 'L3', L3, 'N3', N3, 'T_0', T_0, ...
                'T_end', T_end, 'heat_rate', heat_rate, ...
-               'lambda_test_setup', lambda_test_setup,};
+               'lambda_test_setup', lambda_test_setup, ...
+               'c_p_sample', c_p_sample};
+
+     
 p_sim = get_param_sim(common_args{:}, 'c_p_sample', c_p_sample);
 p_sim = update_c_p(p_sim, p_optim_start);
 
 % choose free(true)/fixed(false) parameters to optimize
 p_optim_estimable = true(length(p_optim_start), 1);
 p_optim_estimable(end-2:end) = false; % fix mapping dT -> dU
-p_optim_estimable(1:length(knots)) = false; % fix knot positions
+p_optim_estimable(1:num_cntrl_pts) = false; % fix x-position of control pts
+p_optim_estimable(2*num_cntrl_pts+1:2*num_cntrl_pts+length(knots)) = false; % fix knot vector
 %p_optim_estimable(15:40) = true; % set knots at pos 135:160 free
 
-n_free_knots = length(15:40);
 
 p_optim_fixed = p_optim_start(~p_optim_estimable);
 
@@ -73,6 +82,8 @@ figure(1); % dU plot
 ax1 = gca();
 figure(2); % c_p plot
 ax2 = gca();
+
+
 
 if strcmp(optim_solverName, 'lsqnonlin')
     residuum_scalar_output = false;
@@ -85,19 +96,21 @@ compute_residuum_expl = @(p_optim) ...
     compute_residuum(p_optim, p_optim_estimable, p_optim_fixed, p_sim, ...
                      U_dsc, c_p_meas, residuum_scalar_output, ax1, ax2);
 
+                 
 % TEST INITIAL VALUE
 % compute_residuum_expl(p_optim_start(p_optim_estimable));
 % return
 
 
 if strcmp(optim_solverName, 'lsqnonlin')
-    lb = zeros(size(coeffs));
-    ub = ones(size(coeffs))*100.;
-    
+    lb = zeros(1,num_cntrl_pts);
+    ub = ones(1,num_cntrl_pts)*100.;
     optim_con = {lb, ub};
 
-    opt_options = optimoptions('lsqnonlin', 'Display', 'iter-detailed', 'OutputFcn', @disp_aux);
-    [p_optim,~,~,~,optim_output,~,jac_output] = lsqnonlin(...
+    opt_options = optimoptions('lsqnonlin', ...
+                               'Display', 'iter-detailed', ...
+                               'OutputFcn', @disp_aux);
+    [p_optim,~,~,~,optim_output] = lsqnonlin(...
         compute_residuum_expl, p_optim_start(p_optim_estimable), optim_con{:}, opt_options);
 
 elseif strcmp(optim_solverName, 'fminsearch')
@@ -140,7 +153,7 @@ elseif strcmp(optim_solverName, 'fmincon')
 else
     error('Choose optim_solverName from [''lsqnonlin'', ''fminsearch'', ''fmincon'']!');
 end
-    
+
 
 % update all (free and fixed) optimization parameters with optimized values
 p_optim_all = zeros(1,length(p_optim_estimable));
@@ -156,5 +169,4 @@ save_path = '/home/argo/masterarbeit/fits_data/';
 fit_data = save_fit(save_path, dsc, index_T_dsc, revMassNorm, ...
     p_sim, optim_solverName, opt_options, p_optim_start, p_optim_estimable, ...
     optim_con, p_optim_all, optim_output);
-
 
