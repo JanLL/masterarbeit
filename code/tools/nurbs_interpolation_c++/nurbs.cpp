c1b5b3cdd4f6170d12b4b6f5ecb7f9c36f3c7234
JanLL <lammel@stud.uni-heidelberg.de>
2017-09-07 01:36:03 +0200

    c++ nurbs computation much faster now because just evaluating necessary sum terms

--- code/tools/nurbs_interpolation_c++/nurbs.cpp
+++ code/tools/nurbs_interpolation_c++/nurbs.cpp
@@ -7,17 +7,19 @@
 
 #include "nurbs.hpp"
 
+//#include <adolc/adouble.h>
+
 
 
 Nurbs::Nurbs(int num_cntrl_pts_in, int nurbs_order_in) :
 	num_cntrl_pts(num_cntrl_pts_in), 
 	nurbs_order(nurbs_order_in),
+	len_U(num_cntrl_pts + nurbs_order),
 	cntrl_pts_x(num_cntrl_pts_in, 0),
 	cntrl_pts_y(num_cntrl_pts_in, 0),
 	weights(num_cntrl_pts_in, 1.),
 	U(num_cntrl_pts_in + nurbs_order_in)
 {
-	int len_U = num_cntrl_pts + nurbs_order;
 
 	for (int i=0; i <= nurbs_order; ++i) {
 		U[i] = 0;
@@ -27,6 +29,18 @@ Nurbs::Nurbs(int num_cntrl_pts_in, int nurbs_order_in) :
 	for (int i=1; i <= num_cntrl_pts - nurbs_order; ++i) {
 		U[i + nurbs_order - 1] = i / float(num_cntrl_pts - nurbs_order + 1);
 	}
+}
+
+
+int Nurbs::get_interval_index(double u) {
+
+	int i = nurbs_order-1;
+	for (int j=nurbs_order-1; j <= len_U-1-nurbs_order; ++j) {
+		i = (u - U[j] > 0) ? j : i;
+	}
+
+	return i;
+
 }
 
 
@@ -91,7 +105,7 @@ double Nurbs::eval_basis_fcn(double u, int i, int p) {
 }
 
 
-void Nurbs::eval_nurbs_curve(double u, std::vector<double>& C) {
+std::vector<double> Nurbs::eval_nurbs_curve(double u) {
 
 	std::vector<double> N_vec(num_cntrl_pts);
 
@@ -106,22 +120,102 @@ void Nurbs::eval_nurbs_curve(double u, std::vector<double>& C) {
 	double numerator_y = 0;
 
 	for (int i=0; i < num_cntrl_pts; ++i) {
-
 		numerator_x += N_vec[i] * weights[i] * cntrl_pts_x[i];
 		numerator_y += N_vec[i] * weights[i] * cntrl_pts_y[i];
 		denominator += N_vec[i] * weights[i];
 
 	}
 
+	std::vector<double> C(2);
 	C[0] = numerator_x / denominator;
 	C[1] = numerator_y / denominator;
 
-	return;
+	return C;
+
+}
+
+double Nurbs::compute_a_i_m3(int i, double u) {
+
+	double a_i_m3;
+
+	a_i_m3 = (U[i+4] - u)*(U[i+4] - u)*(U[i+4] - u) / ((U[i+4] - U[i+1])*(U[i+4] - U[i+2])*(U[i+4] - U[i+3]));
+
+	return a_i_m3;
 
 }
 
 
-/*
+double Nurbs::compute_a_i_m2(int i, double u) {
+
+	double a_i_m2;
+
+	a_i_m2 = (u - U[i])/(U[i+3] - U[i]) * (U[i+3] - u)/(U[i+3] - U[i+1]) * (U[i+3] - u)/(U[i+3] - U[i+2]) + 
+			 (U[i+4] - u)/(U[i+4] - U[i+1]) * ( (u - U[i+1])/(U[i+3] - U[i+1]) * (U[i+3] - u)/(U[i+3] - U[i+2])
+			                                   +(U[i+4] - u)/(U[i+4] - U[i+2]) * (u - U[i+2])/(U[i+3] - U[i+2]) );
+
+	return a_i_m2;
+
+}
+
+double Nurbs::compute_a_i_m1(int i, double u) {
+
+	double a_i_m1;
+
+	a_i_m1 = (u - U[i])/(U[i+3] - U[i]) * ( (u - U[i])/(U[i+2] - U[i]) * (U[i+2] - u)/(U[i+2] - U[i+1])
+			                               +(U[i+3] - u)/(U[i+3] - U[i+1]) * (u - U[i+1])/(U[i+2] - U[i+1]) )
+	         + (U[i+4] - u)/(U[i+4] - U[i+1]) * (u - U[i+1])/(U[i+3] - U[i+1]) * (u - U[i+1])/(U[i+2] - U[i+1]);
+
+	return a_i_m1;
+
+}
+
+double Nurbs::compute_a_i_0(int i, double u) {
+
+	double a_i_0;
+
+	a_i_0 = (u - U[i])*(u - U[i])*(u - U[i]) / ((U[i+3] - U[i])*(U[i+2] - U[i])*(U[i+1] - U[i]));
+
+	return a_i_0;
+
+}
+
+
+std::vector<double> Nurbs::eval_nurbs_curve_mod(double u) {
+
+	int i = this->get_interval_index(u);
+
+	double a_im3_m3 = this->compute_a_i_m3(i-3, u);
+	double a_im2_m2 = this->compute_a_i_m2(i-2, u);
+	double a_im1_m1 = this->compute_a_i_m1(i-1, u);
+	double a_i_0 = this->compute_a_i_0(i, u);
+
+
+	double numerator_x = a_im3_m3 * weights[i-3] * cntrl_pts_x[i-3] +
+						 a_im2_m2 * weights[i-2] * cntrl_pts_x[i-2] +
+						 a_im1_m1 * weights[i-1] * cntrl_pts_x[i-1] +
+						 a_i_0    * weights[i-0] * cntrl_pts_x[i-0];
+
+	double numerator_y = a_im3_m3 * weights[i-3] * cntrl_pts_y[i-3] +
+						 a_im2_m2 * weights[i-2] * cntrl_pts_y[i-2] +
+						 a_im1_m1 * weights[i-1] * cntrl_pts_y[i-1] +
+						 a_i_0    * weights[i-0] * cntrl_pts_y[i-0];
+
+	double denominator = a_im3_m3 * weights[i-3] +
+						 a_im2_m2 * weights[i-2] +
+						 a_im1_m1 * weights[i-1] +
+						 a_i_0    * weights[i-0];
+
+	std::vector<double> C(2);
+	C[0] = numerator_x / denominator;
+	C[1] = numerator_y / denominator;
+
+	return C;
+
+}	
+
+
+
+
 int main(int argc, char** argv) {
 
 	int nurbs_order = 4;
@@ -129,6 +223,10 @@ int main(int argc, char** argv) {
 
 	std::vector<double> cntrl_pts_x = {0, 30, 60, 90, 120, 125, 130, 132., 135, 150, 160, 180};
 	std::vector<double> cntrl_pts_y = {1., 1,  1.1, 1.15, 1.2, 5., 10, 1.5, 1.51, 1.52, 1.53, 1.54};
+	//std::vector<double> cntrl_pts_x = {0, 1, 2, 3, 4, 5, 6, 7};
+	//std::vector<double> cntrl_pts_y = {0, 1, 2, 3, 4, 5, 6, 7};
+	
+
 	int num_cntrl_pts = cntrl_pts_x.size();
 
 
@@ -136,6 +234,28 @@ int main(int argc, char** argv) {
 	nurbs.set_cntrl_pts_x(cntrl_pts_x);
 	nurbs.set_cntrl_pts_y(cntrl_pts_y);
 
+	/*std::vector<double> U = nurbs.get_U();
+	for (std::vector<double>::iterator it=U.begin(); it!=U.end(); ++it) {
+		std::cout << *it << "\t";
+	}
+	std::cout << std::endl;*/
+
+
+	std::ofstream file_nurbes;
+  	file_nurbes.open ("curve_nurbes.txt");
+
+  	for (double u=0.; u<1; u+=0.001) {
+
+		std::vector<double> C = nurbs.eval_nurbs_curve(u);
+		std::vector<double> D = nurbs.eval_nurbs_curve_mod(u);
+		
+		file_nurbes << C[0] << "\t" << C[1] << "\t" << D[0] << "\t" << D[1] << std::endl;
+
+	}
+	file_nurbes.close();
+
+	}/*
+	
 
 	double h = 0.01;
 
--- code/tools/nurbs_interpolation_c++/nurbs.hpp
+++ code/tools/nurbs_interpolation_c++/nurbs.hpp
@@ -24,12 +24,23 @@ class Nurbs
 		double eval_basis_fcn(double u, int i, int p);
 
 
-		void eval_nurbs_curve(double u, std::vector<double>& C);
+		std::vector<double> eval_nurbs_curve(double u);
+
+		int get_interval_index(double u);
+
+		double compute_a_i_m3(int i, double u);
+		double compute_a_i_m2(int i, double u);
+		double compute_a_i_m1(int i, double u);
+		double compute_a_i_0(int i, double u);
+
+		std::vector<double> eval_nurbs_curve_mod(double u);
+
 
 
 	private:
 		int num_cntrl_pts;
 		int nurbs_order;
+		int len_U;
 
 		std::vector<double> cntrl_pts_x;
 		std::vector<double> cntrl_pts_y;
