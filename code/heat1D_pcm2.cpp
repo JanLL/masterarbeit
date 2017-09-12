7f987ee109e7f6729c8dc1855c3314e1b383fc21
JanLL <lammel@stud.uni-heidelberg.de>
2017-09-13 01:34:23 +0200

    continued heat1D_pcm2.cpp

--- code/heat1D_pcm2.cpp
+++ code/heat1D_pcm2.cpp
@@ -88,68 +88,64 @@ std::vector< Sonic::DMat> g_fwdSens;
 std::vector< Sonic::DMat> g_adjSens;
 
 
-const long maxLevels = 2; // Comment: for more levels, the boost preprocessor subset might be helpful
+double L1;				// Length of Constantan
+double L3;				// Length of PCM
+long N1;				// Number of discretization points Constantan
+long N3;				// Number of discretization points PCM
+double dx_Const;		// Mesh size Constantan
+double dx_pcm;			// Mesh size PCM
+double alpha;			// quotient for Constantan / PCM grid transition
 
-double L1;					// Length of Constantan
-double L3;					// Length of PCM
-long N1[maxLevels];			// Number of discretization points Constantan
-long N3[maxLevels];			// Number of discretization points PCM
-double dx_const[maxLevels];	// Mesh size Constantan
-double dx_pcm[maxLevels];	// Mesh size PCM
-double alpha;				// quotient for Constantan / PCM grid transition
+double lambda_Const;
+double rho_Const;
+double c_p_Const;
+double lambda_pcm;
+double rho_pcm;
 
-template <typename T, long level>
+double heat_rate;
+double T_0;
+double T_end;
+
+
+template <typename T>
 svLong diffRHS(TArgs_ffcn<T> &args, TDependency *depends)
 {
-
-	// parameters
-	T a_const    = args.p[0];		// Temp.-conductivity (diffusion constant) of Constantan [mm^2/s] 
-	T lambda_pcm = args.p[1];		// heat conductivity of pcm [mW/(mm*K)]
-	T rho_pcm    = args.p[2];		// density of pcm [mg/mm^3]
-	T heat_rate  = args.p[3];		// rate [K/min] with which oven temperature increases.
-
-	T h          = args.p[4];
-	T r          = args.p[5];
-	T wr         = args.p[6];
-	T sr         = args.p[7];
-	T z          = args.p[8];
-	T b          = args.p[9];
-
 
 
 	const T* x = args.xd;
 
 	// some pre-calculations
+	T a_Const      = lambda_Const / (c_p_Const * rho_Const);
 	T heat_rate_s  = heat_rate / 60; // [K/min] -> [K/s]
-	T scale_Const  = a_const / (dx_const[level]*dx_const[level]);
-	T scale_pcm    = lambda_pcm / (rho_pcm * dx_pcm[level]*dx_pcm[level]);
+	T scale_Const  = a_Const / (dx_Const*dx_Const);
+	T scale_pcm    = lambda_pcm / (rho_pcm * dx_pcm*dx_pcm);
 
 	/******************* Building up RHS of ODE ********************/
 	// Oven boundary with constant slope of Temp.
 	args.rhs[0] = heat_rate_s;
 	
 	// Constantan (just linear part)
-	for (long j = 1; j <= N1[level]-2; j++)
+	for (long j = 1; j <= N1-2; j++)
 	{
 		args.rhs[j] = scale_Const * (x[j-1] - 2.0 * x[j] + x[j+1]); 
 	}
 
 
 	// Intermediate area between Constantan and PCM, belongs to Constantan
-	long j = N1[level]-1;
+	long j = N1-1;
 	args.rhs[j] = scale_Const * (2./(1.+alpha) * x[j-1] - 2./alpha * x[j] + 2./(alpha*(alpha+1.)) * x[j+1]);
 
 
 	// PCM
 	T c_p_j;
 	T dc_p_j;
-	for (long j = N1[level]; j <= N1[level]+N3[level]-2; j++)
+	for (long j = N1; j <= N1+N3-2; j++)
 	{
 		// c_p calculation using Fraser-Suzuki Peak
-		//fraser_suzuki(x[j], c_p_j, dc_p_j, h, r, wr, sr, z, b);
+		//fraser_suzuki(x[j], c_p_j, dc_p_j, args.p[0], args.p[1], args.p[2], args.p[3], args.p[4], args.p[5]);
 	
 		// c_p calculation using old atan formula
-		c_p_formula(x[j], c_p_j, dc_p_j, args.p[4], args.p[5], args.p[6], args.p[7], args.p[8], args.p[9]);
+		c_p_formula(x[j], c_p_j, dc_p_j, args.p[0], args.p[1], args.p[2], args.p[3], args.p[4], args.p[5]);
 
 		// linear part 
 		args.rhs[j] = scale_pcm/c_p_j * (x[j-1] - 2.0 * x[j] + x[j+1]);
@@ -159,7 +155,7 @@ svLong diffRHS(TArgs_ffcn<T> &args, TDependency *depends)
 	}
 
 	// RHS boundary, no flux
-	args.rhs[N1[level]+N3[level]-1] = scale_pcm * (x[N1[level]+N3[level]-2] - x[N1[level]+N3[level]-1]); // Neumann boundary (right)
+	args.rhs[N1+N3-1] = scale_pcm * (x[N1+N3-2] - x[N1+N3-1]); // Neumann boundary (right)
 
 
 	return 0;
@@ -174,12 +170,34 @@ int main( int argc, char* argv[] )
 	svLong errorCode ( 0 );
 
 
+	// Set Parameters
+	L1 = 15.;
+	L3 = 0.5;
+	N1 = 200;
+	N3 = 50;
+
+	lambda_Const = 23.;		
+	rho_Const    = 8.9;
+	c_p_Const    = 0.41;
+
+	lambda_pcm   = 0.96;	// heat conductivity of pcm [mW/(mm*K)]
+	rho_pcm      = 0.85;	// density of pcm [mg/mm^3]
+
+	heat_rate    = 10.;     // Oven temp heat rate [K/min]   
+	T_0  	     = 10.;     // [degC]
+	T_end  	     = 200.;    // [degC]
+	
+
+	// Some pre-calculations
+	double heat_rate_s = heat_rate / 60.; // [K/min] -> [K/s]
+
+
 	// create a DAESOL-II instance
 	IIntegrator * integrator = IIntegrator::create( IIntegrator::Impl_DAESOL_II );
 
 	// get corresponding integrator options and evaluator from the integrator instance
-	IOptions   *intOptions   = integrator-> getOptions();
-	TIntegratorEvaluator *evaluator    = integrator-> getEvaluator();
+	IOptions             *intOptions = integrator-> getOptions();
+	TIntegratorEvaluator *evaluator  = integrator-> getEvaluator();
 
 
 	// choose the modules used by DAESOL-II by passing the corresponding options (ATLAS or UMFPACK for linear algebra)
@@ -209,13 +227,67 @@ int main( int argc, char* argv[] )
 
 	// create and configure dimensions
 	TIntegratorDimensions dims;
-	dims. dim[ Component_XD ] = 2;
+	dims. dim[ Component_XD ] = N1+N3;
 	dims. dim[ Component_XA ] = 0;
-	dims. dim[ Component_U  ] = 1;
-	dims. dim[ Component_P  ] = 1;
-	dims. dim[ Component_Q  ] = 1;
-	dims. dim[ Component_H  ] = 1;
+	dims. dim[ Component_U  ] = 0;
+	dims. dim[ Component_P  ] = 6;
+	dims. dim[ Component_Q  ] = 0;
+	dims. dim[ Component_H  ] = 0;
 	dims. nTrajectories       = 1;
+
+
+
+	svULong nxd     = dims. dim [ Component_XD ];
+	svULong np      = dims. dim [ Component_P  ];
+
+
+	// pass problem dimensions to evaluator and integrator
+	evaluator->  setDimensions ( dims );
+	integrator-> setDimensions ( dims );
+
+	// create model function class and fill it with the rhs pointer and pass it to the evaluator
+	TModelFunctions model;
+	model. setFunction < Function_ffcn > ( diffRHS <double>  );
+	model. setFunction < Function_ffcn > ( diffRHS <adouble>  );
+
+	evaluator->setModelFunctions ( &model );
+	integrator->setIntegratorPrintLevel( INTEGRATOR_PRINT_LVL );
+
+
+	const double t_0
+	const double t_end = (T_end - T_0) / heat_rate_s;
+
+	evaluator->setTimeHorizon( 0, t_end );
+
+
+	double* init_xd = new double [ nxd ];
+	for (int i=0; i<nxd; ++i) {
+		init_xd[i] = T_0;
+	}
+
+	double* params = new double [ np ];
+	params[0] = 125.;
+	params[1] = 10.;
+	params[2] = 0.01;
+	params[3] = 10.;
+	params[4] = 0.003;
+	params[5] = 2.;
+
+	integrator->setInitialValues(
+			Component_XD,
+			nxd, // leading dim of init_xd
+			init_xd
+			);
+	integrator->setInitialValues(
+			Component_P ,
+			np,
+			&param
+			);
+
+
+
+
+
 
 
 
